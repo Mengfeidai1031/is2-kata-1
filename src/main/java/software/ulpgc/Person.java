323d90019ac0e29180303e51e6608d507aa1a555
mengfei-ana <mengfeidai713@gmail.com>
2025-01-18 17:51:04 +0000

    Initial commit

---
+++ src/main/java/software/ulpgc/Person.java
@@ -0,0 +1,39 @@
+package software.ulpgc;
+
+import java.time.LocalDate;
+
+public class Person {
+    private final String name;
+    private final LocalDate birthday;
+
+
+    public Person(String name, LocalDate birthday) {
+        this.name = name;
+        this.birthday = birthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public int getAge() {
+        return toYear(LocalDate.now().getYear() - birthday.getYear());
+    }
+
+    private int toYear(int days) {
+        return (int) days / 365;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", birthday=" + birthday +
+                ", age=" + getAge() +
+                '}';
+    }
+}
